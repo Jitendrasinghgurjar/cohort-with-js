2d00f014aa0d2416c8eea9b08aaf457833fb6a3f
jitendrasinghgurjar <23jitugurjar@gmail.com>
2025-09-25 12:14:28 +0530

    Introduction to javascript

---
+++ 01-introduction-to-javascript/script.js
@@ -0,0 +1,63 @@
+// // // // // // var a = 10;
+// // // // // // var a = 20
+// // // // // // console.log(a);
+// // // // // //ham var ke sath redclare and reaasign kar sakte ha 
+
+
+// // // // // let a = 10
+// // // // // let a = 50
+// // // // // console.log(a);
+// // // // // console.log(a);
+// // // // // //ham let ke sath redclare nahi kar sakte ha par reasign  kar sakte ha
+
+// // // // const a = 200
+// // // // a = 400
+// // // // const a = 600
+// // // // console.log(a);
+
+// // // // //ham const variable ke sath redclare bi nahi kar sakte and reasign bi nahi kar sakte
+
+// // // //ab ham padhenge dynamic language 
+// // // //dynamic language = dynamic langauge ka matlab ha ki aisi language jisme hame data ka type define nahi karna pdatha wo kehlati ha dynamic langauge
+// // // //static langauge = aisi langauge jisme data ka type define karna pade use ham static langauge bolte ha
+
+// // //     //javascipt me teen dialog boxes hote ha unka naam ha
+// // //     // 1 = alert
+// // //     // 2 = prompt
+// // //     // 3 = confirm
+
+// // //    let you =  alert("are you mast")
+// // //    console.log(alert);
+   
+// // //     //alert ye message show karta ha    
+// // // let jitu = prompt("kya tumhe job chaiye")
+// // // console.log(jitu);
+// // // //prompt user se input leta ha 
+
+// // // let yaar = confirm("are you 18+")
+// // // console.log(yaar);
+// // // //ye user se confirmation leta ha
+// // // operator
+// // let a = 10
+// // let b =5
+// // console.log(a+b);
+// // console.log(a-b);
+// // console.log(a*b);
+// // console.log(a/b);
+// // console.log(a%b);
+
+// //increment operator and // decrement operator
+// let a = 10
+// a++
+// console.log(a++);
+// console.log(a);
+
+// let b = 10
+// b--
+// console.log(b++);
+// console.log(b--);
+// console.log(b);
+
+
+//javascript is a interpretred langauge
+
